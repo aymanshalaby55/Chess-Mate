f2daf1643bf5865eefd6ce03a514dbb6361e36c2
Ayman Shalaby <aymanshalaby539@gmail.com>
2025-05-17 11:17:27 +0300

    fix: lint error

--- apps/api/src/game/game.module.ts
+++ apps/api/src/game/game.module.ts
@@ -1,12 +1,12 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
 
-import { PrismaService } from "src/prisma/prisma.service";
-import { GameController } from "./game.controller";
-import { GameService } from "./game.service";
+import { PrismaService } from 'src/prisma/prisma.service';
+import { GameController } from './game.controller';
+import { GameService } from './game.service';
 
 @Module({
-    controllers: [GameController],
-    providers: [GameService, PrismaService],
-    exports: [GameService],
+  controllers: [GameController],
+  providers: [GameService, PrismaService],
+  exports: [GameService],
 })
 export class GameModule {}
