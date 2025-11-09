cd4fb65e32d67a0bcfc16b79387d7ffe95eaa762
Ahmed Wagih <113104074+AhmeWagih@users.noreply.github.com>
2025-11-09 19:57:11 +0200

    last version

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -2,6 +2,17 @@ import Hero from '@/components/shared/Hero';
 
 export default function Home() {
   return (
+    /**
+     TESTING:
+     - Hero component
+     - PlayNow component
+     - Chessboard component
+     - SignInButton component
+     - SignUpButton component
+     - SignOutButton component
+     - SignInButton component
+     - SignUpButton component
+     */
     <div>
       <Hero />
     </div>
